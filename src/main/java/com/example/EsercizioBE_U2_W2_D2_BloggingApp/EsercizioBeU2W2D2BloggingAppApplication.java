39fb1e4a0e1de9b4dee437d4c5c5b4522ddea99b
Edoardo <edfl.tunzi@gmail.com>
2025-02-11 14:48:49 +0100

    initial commit

---
+++ src/main/java/com/example/EsercizioBE_U2_W2_D2_BloggingApp/EsercizioBeU2W2D2BloggingAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.EsercizioBE_U2_W2_D2_BloggingApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EsercizioBeU2W2D2BloggingAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EsercizioBeU2W2D2BloggingAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/EsercizioBE_U2_W2_D2_BloggingApp/EsercizioBeU2W2D2BloggingAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.EsercizioBE_U2_W2_D2_BloggingApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EsercizioBeU2W2D2BloggingAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
