1e7d8170cdeb883b1956846892b1202a71c877d2
Edoardo <edfl.tunzi@gmail.com>
2025-02-12 16:48:16 +0100

    primi due metodi di Autore controller

--- src/main/java/com/example/EsercizioBE_U2_W2_D2_BloggingApp/controller/AutoreController.java
+++ src/main/java/com/example/EsercizioBE_U2_W2_D2_BloggingApp/controller/AutoreController.java
@@ -1,62 +1,28 @@
 package com.example.EsercizioBE_U2_W2_D2_BloggingApp.controller;
 
+import com.example.EsercizioBE_U2_W2_D2_BloggingApp.model.Autore;
 import com.example.EsercizioBE_U2_W2_D2_BloggingApp.model.Post;
+import com.example.EsercizioBE_U2_W2_D2_BloggingApp.service.AutoreService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/autore")
 public class AutoreController {
+    @Autowired
+    AutoreService autoreService;
 
-    /*@GetMapping("/")
-    public List<Post> getAllPost() {
-       *//* return blogPosts;*//*
+    @GetMapping
+    public List<Autore> getAllPost() {
+        return autoreService.getAllAutori();
     }
 
     @GetMapping("/{id}")
-    public Post getPostById(@PathVariable int id) {
-        *//*return blogPosts.stream()
-                .filter(post -> post.getId() == id)
-                .findFirst().get();*//*
+    public Optional<Autore> getAutoreById(@PathVariable long id) {
+        return autoreService.getAutoreById(id);
     }
-
-    @PostMapping("/")
-    @ResponseStatus(HttpStatus.CREATED)
-    public String newPost(@RequestBody Post post) {
-        *//*String descrizione = "Dettaglio post: \n";
-        descrizione += "Id: " + post.getId() + "\n";
-        descrizione += "categoria: " + post.getCategoria() + "\n";
-        descrizione += "titolo: " + post.getTitolo() + "\n";
-        descrizione += "contenuto: " +post.getContenuto() + "\n";
-        descrizione += "tempo di lettura" + post.getTempoDiLettura() + "\n";
-        blogPosts.add(new Post(post.getCategoria(),post.getTitolo(),post.getContenuto(),post.getTempoDiLettura()));
-        return descrizione;*//*
-    }
-
-    @PutMapping("/{id}")
-    @ResponseStatus(HttpStatus.OK)
-    public Post updatePost(@PathVariable int id, @RequestBody Post post) {
-       *//* Post postModificare = blogPosts.stream()
-                .filter(blogpost -> blogpost.getId() == id)
-                .findFirst().get();
-
-        if (postModificare != null) {
-            postModificare.setTitolo(post.getTitolo());
-            postModificare.setCategoria(post.getCategoria());
-            postModificare.setContenuto(post.getContenuto());
-            postModificare.setTempoDiLettura(post.getTempoDiLettura());
-        }
-        return postModificare;*//*
-    }
-
-    @DeleteMapping("/{id}")
-    public String updatePost(@PathVariable int id) {
-       *//* Post postModificare = blogPosts.stream()
-                .filter(blogpost -> blogpost.getId() == id)
-                .findFirst().get();
-        blogPosts.remove(postModificare);
-        return "Post cancellato";*//*
-    }*/
 }
--- src/main/java/com/example/EsercizioBE_U2_W2_D2_BloggingApp/service/AutoreService.java
+++ src/main/java/com/example/EsercizioBE_U2_W2_D2_BloggingApp/service/AutoreService.java
@@ -1,11 +1,54 @@
 package com.example.EsercizioBE_U2_W2_D2_BloggingApp.service;
 
+import com.example.EsercizioBE_U2_W2_D2_BloggingApp.dto.PostDTO;
+import com.example.EsercizioBE_U2_W2_D2_BloggingApp.model.Autore;
 import com.example.EsercizioBE_U2_W2_D2_BloggingApp.repository.AutoreDAORepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class AutoreService {
     @Autowired
     AutoreDAORepository autoreRepo;
+    public List<Autore> getAllAutori() {
+        return autoreRepo.findAll();
+    }
+
+    public Optional<Autore> getAutoreById(long id) {
+        Optional<Autore> autore = autoreRepo.findById(id);
+        if(autore.isPresent()) {
+            return autore;
+        } else {
+            throw new RuntimeException("Autore non trovato nel DB");
+        }
+    }
+
+
+    public String addAutore(Autore autore) {
+        autoreRepo.save(autore);
+        return "Autore con id: " + autore.getId() + "salvato nel db";
+    }
+
+    /*public Autore updateAutoreById(long id, Autore autore) {
+        Autore result = getAutoreById(id).get();
+        result.setAutore(post.getAutore());
+        result.setTitolo(post.getTitolo());
+        result.setCategoria(post.getCategoria());
+        result.setContenuto(post.getContenuto());
+        result.setTempoDiLettura(post.getTempoDiLettura());
+        return result;
+    }*/
+
+    public void deleteAutoreById(long id) {
+        Optional<Autore> result = getAutoreById(id);
+        if (result.isPresent()) {
+            autoreRepo.deleteById(id);
+        } else {
+            throw new RuntimeException("Nessun utente con questo id da poter eliminare");
+        }
+
+    }
 }
